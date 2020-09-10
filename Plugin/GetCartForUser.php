38b46063bcf274b150458750c993c49230f064df
Cody Nguyen <cody@simicart.com>
2020-09-10 17:16:25 +0700

    Switch cart currency + store id when its different with current store code and currency

--- Plugin/GetCartForUser.php
+++ Plugin/GetCartForUser.php
@@ -1,15 +1,22 @@
 <?php
 namespace Simi\SimiconnectorGraphQl\Plugin;
 
-use Magento\Quote\Api\CartRepositoryInterface;
-use Magento\Quote\Model\MaskedQuoteIdToQuoteIdInterface;
+use \Magento\Quote\Api\CartRepositoryInterface;
+use \Magento\Quote\Model\MaskedQuoteIdToQuoteIdInterface;
+use \Magento\Store\Model\StoreManagerInterface;
 
 class GetCartForUser
 {
+    private $storeManager;
+    private $maskedQuoteIdToQuoteId;
+    private $cartRepository;
+
     public function __construct(
+    	StoreManagerInterface $storeManager,
         MaskedQuoteIdToQuoteIdInterface $maskedQuoteIdToQuoteId,
         CartRepositoryInterface $cartRepository
     ) {
+        $this->storeManager = $storeManager;
         $this->maskedQuoteIdToQuoteId = $maskedQuoteIdToQuoteId;
         $this->cartRepository = $cartRepository;
     }
@@ -19,9 +26,18 @@ class GetCartForUser
         try {
             $cartId = $this->maskedQuoteIdToQuoteId->execute($cartHash);
             $cart = $this->cartRepository->get($cartId);
+            $cartChanged = false;
         	if ((int)$cart->getStoreId() !== $storeId) {
-        		$cart->setStoreId($storeId)->save();
+            	$cartChanged = true;
+        		$cart->setStoreId($storeId);
         	}
+        	$currencyCode  = $this->storeManager->getStore()->getCurrentCurrencyCode();
+        	if ($cart->getData('quote_currency_code') !== $currencyCode) {
+            	$cartChanged = true;
+        		$cart->setData('quote_currency_code', $currencyCode);
+        	}
+        	if ($cartChanged)
+        		$cart->save();
         } catch (\Exception $e) {
             
         }
