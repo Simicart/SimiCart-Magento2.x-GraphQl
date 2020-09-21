720cbc852f1c238ee1a37251714d2dc35179f0c8
duong <innerpeace1803@gmail.com>
2020-09-22 01:39:31 +0700

    fix the QR code in resolver search by name

--- Model/Resolver/QRproduct.php
+++ Model/Resolver/QRproduct.php
@@ -11,30 +11,29 @@ use Magento\Sales\Model\ResourceModel\Report\Bestsellers\CollectionFactory as Be
 use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory;
 use Magento\CatalogGraphQl\DataProvider\Product\SearchCriteriaBuilder;
 use Simi\SimiconnectorGraphQl\Model\Resolver\Products\Query\Search;
+use Magento\CatalogGraphQl\Model\Resolver\Products\DataProvider\Deferred\Product as ProductDataProvider;
 
 
 class QRproduct implements ResolverInterface
 {
 
-    protected $storeManager;
     protected $imageHelper;
     protected $qrcollection;
     protected $productModel;
     protected $simibarcodeModel;
     protected $_productRepository;
     protected $searchApiCriteriaBuilder;
+    private $searchQuery;
 
     public function __construct(
-        \Magento\Store\Model\StoreManagerInterface $storeManager,
         \Simi\Simiconnector\Helper\Data $helper,
         \Magento\Catalog\Model\Product $productModel,
         \Simi\Simiconnector\Model\ResourceModel\Simibarcode\CollectionFactory $QRcollection,
         \Simi\Simiconnector\Model\Simibarcode $simibarcodeModel,
         \Magento\Catalog\Model\ProductRepository $productRepository,
         SearchCriteriaBuilder $searchApiCriteriaBuilder = null,
-        Search $searchQuery
+        Search $searchQuery,
     ) {
-        $this->storeManager = $storeManager;
         $this->imageHelper = $helper;
         $this->productModel = $productModel;
         $this->qrcollection = $QRcollection;
@@ -47,39 +46,35 @@ class QRproduct implements ResolverInterface
 
     public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null)
     {
-        if($args['barcode']){
+        if(isset($args['barcode'])){
             // die(var_dump($args['barcode']));
             $collection = $this->qrcollection->create();
             $result = $this->simibarcodeModel->getCollection()->addFieldToFilter('barcode_status', '1')->addFieldToFilter('barcode', $args['barcode'])->getData()[0];
         }
-        else if($args['qrcode']){
+        else if(isset($args['qrcode'])){
             $collection = $this->qrcollection->create();
             $result = $this->simibarcodeModel->getCollection()->addFieldToFilter('barcode_status', '1')->addFieldToFilter('qrcode', $args['qrcode'])->getData()[0];
         }
-        // die(var_dump(get_class_methods($info)));
-        //data to search for the product
-        $sku = [
-            'filter' => [
-                'sku' => [
-                    'eq' => $result['product_sku']
-                ] 
-            ],
-            'pageSize' => 20,
-            'currentPage' => 1
-        ];
-        //all information of the product
+         // die(var_dump($result));
 
         /* $productInfo = $product->loadByAttribute('sku', $result['product_sku'])->getData();
         $_product = $this->getProductBySku($result['product_sku']);
         $attributes = $_product->getAttributes();// All Product Attributes */
         // die(var_dump(json_decode(json_encode($attributes))));
-        
+        $name = [
+            'filter'=> [
+                'name'=>[
+                    'match' => $result['product_name']
+                ]
+            ],
+            'pageSize'=>20,
+            'currentPage' =>1
+        ];
         $includeAggregations = false;
-        $searchCriteria = $this->searchApiCriteriaBuilder->build($sku, $includeAggregations);
-        $searchResult = $this->searchQuery->getResult($searchCriteria, $info, $sku);
-        $products = $searchResult->getProductsSearchResult();
         
-
+        $searchCriteria = $this->searchApiCriteriaBuilder->build($name, $includeAggregations);
+        $searchResult = $this->searchQuery->getResult($searchCriteria, $info, $name);
+        $products = $searchResult->getProductsSearchResult();
         foreach ($products as $product){
             $finalResult =
             [
