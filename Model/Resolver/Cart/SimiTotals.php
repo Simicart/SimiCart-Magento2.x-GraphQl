98333aa39426c15e9cb9db6cc1e8fccac392d2d6
Cody Nguyen <cody@simicart.com>
2021-04-29 11:15:21 +0700

    Add category image to avoid 'no image file' exception Add  total to avoid error with wrong total value while using extensions

---
+++ Model/Resolver/Cart/SimiTotals.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Simi\SimiconnectorGraphQl\Model\Resolver\Cart;
+
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Magento\Quote\Model\Quote;
+
+/**
+ * @inheritdoc
+ */
+class SimiTotals implements ResolverInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null)
+    {
+        if (!isset($value['model'])) {
+            throw new LocalizedException(__('"model" value should be specified'));
+        }
+        $quote = $value['model'];
+        $currency = $quote->getData('quote_currency_code');
+        return array(
+            array(
+                'code' => 'grand_total',
+                'value' => $quote->getGrandTotal(),
+                'currency' => $currency,
+            ),
+            array(
+                'code' => 'subtotal',
+                'value' => $quote->getSubtotal(),
+                'currency' => $currency,
+            ),
+            array(
+                'code' => 'subtotal_with_discount',
+                'value' => $quote->getSubtotalWithDiscount(),
+                'currency' => $currency,
+            ),
+        );
+        return null;
+    }
+}
