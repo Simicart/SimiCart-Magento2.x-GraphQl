4616db51c758875821ee2f4fe9550c6f6dc5bb38
Cody Nguyen <cody@simicart.com>
2021-07-14 12:08:13 +0700

    Grid image missing fix (after uncomment di line, run  php bin/magento catalog:images:resize --async)

---
+++ Override/MediaGallery/Url.php
@@ -0,0 +1,122 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Simi\SimiconnectorGraphQl\Override\MediaGallery;
+
+use Magento\Catalog\Model\Product;
+use Magento\Catalog\Model\Product\ImageFactory;
+use Magento\CatalogGraphQl\Model\Resolver\Products\DataProvider\Image\Placeholder as PlaceholderProvider;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+
+/**
+ * Returns media url
+ */
+class Url implements ResolverInterface
+{
+    /**
+     * @var ImageFactory
+     */
+    private $productImageFactory;
+
+    /**
+     * @var PlaceholderProvider
+     */
+    private $placeholderProvider;
+
+    /**
+     * @var string[]
+     */
+    private $placeholderCache = [];
+
+    /**
+     * @param ImageFactory $productImageFactory
+     * @param PlaceholderProvider $placeholderProvider
+     */
+    public function __construct(
+        ImageFactory $productImageFactory,
+        PlaceholderProvider $placeholderProvider
+    ) {
+        $this->productImageFactory = $productImageFactory;
+        $this->placeholderProvider = $placeholderProvider;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function resolve(
+        Field $field,
+        $context,
+        ResolveInfo $info,
+        array $value = null,
+        array $args = null
+    ) {
+        if (!isset($value['image_type']) && !isset($value['file'])) {
+            throw new LocalizedException(__('"image_type" value should be specified'));
+        }
+
+        if (!isset($value['model'])) {
+            throw new LocalizedException(__('"model" value should be specified'));
+        }
+
+        /** @var Product $product */
+        $product = $value['model'];
+        if (isset($value['image_type'])) {
+            $imagePath = $product->getData($value['image_type']);
+            return $this->getImageUrl($value['image_type'], $imagePath);
+        } elseif (isset($value['file'])) {
+            return $this->getImageUrl('image', $value['file']);
+        }
+        return [];
+    }
+
+    /**
+     * Get image URL
+     *
+     * @param string $imageType
+     * @param string|null $imagePath
+     * @return string
+     * @throws \Exception
+     */
+    private function getImageUrl(string $imageType, ?string $imagePath): string
+    {
+        if (empty($imagePath) && !empty($this->placeholderCache[$imageType])) {
+            return $this->placeholderCache[$imageType];
+        }
+        $image = $this->productImageFactory->create();
+        $image->setDestinationSubdir($imageType);
+        if ($imageType === 'small_image') {
+            /* 
+            find the width and height from template xml configuration file, like
+            app/design/frontend/Simi/base_child/etc/view.xml
+            <image id="category_page_grid" type="small_image">
+                <width>350</width>
+                <height>467</height>
+            </image>
+
+            The default value is at
+            vendor/magento/theme-frontend-luma/etc/view.xml
+            <image id="category_page_grid" type="small_image">
+                <width>240</width>
+                <height>300</height>
+            </image>
+            */
+            $image->setWidth('350');
+            $image->setHeight('467');
+        }
+        $image->setBaseFile($imagePath);
+
+        if ($image->isBaseFilePlaceholder()) {
+            $this->placeholderCache[$imageType] = $this->placeholderProvider->getPlaceholder($imageType);
+            return $this->placeholderCache[$imageType];
+        }
+
+        return $image->getUrl();
+    }
+}
